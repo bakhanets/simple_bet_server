d8de103e13d1310ed9afc575c8d990178d4fe202
Semyon Bakhanets <bakhanetssimon@gmail.com>
2022-08-03 00:58:45 +0700

    main file redesigned

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"betting_server/betting_store"
 	"encoding/json"
 	"fmt"
 	"log"
 	"mime"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"betting_server/betting_store"
 )
 
 type taskServer struct {
@@ -23,57 +19,80 @@ func NewTaskServer() *taskServer {
 	return &taskServer{store: store}
 }
 
-func (ts *taskServer) taskHandler(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/task/" {
-		// Request is plain "/task/", without trailing ID.
-		if req.Method == http.MethodPost {
-			ts.createTaskHandler(w, req)
-		} else if req.Method == http.MethodGet {
-			ts.getAllTasksHandler(w, req)
-		} else if req.Method == http.MethodDelete {
-			ts.deleteAllTasksHandler(w, req)
-		} else {
-			http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
-			return
-		}
+func (ts *taskServer) dataHandler(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/getMelbetLink" {
+		ts.getMelbetLink(w, req)
+	} else if req.URL.Path == "/get1XbetLink" {
+		ts.get1xbetLink(w, req)
+	} else if req.URL.Path == "/get1winLink" {
+		ts.get1winLink(w, req)
+	} else if req.URL.Path == "/getReviewValue" {
+		ts.getReviewValue(w, req)
+	} else if req.URL.Path == "/setReviewValue" {
+		ts.setReviewValue(w, req)
 	} else {
-		// Request has an ID, as in "/task/<id>".
-		path := strings.Trim(req.URL.Path, "/")
-		pathParts := strings.Split(path, "/")
-		if len(pathParts) < 2 {
-			http.Error(w, "expect /task/<id> in task handler", http.StatusBadRequest)
-			return
-		}
-		id, err := strconv.Atoi(pathParts[1])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if req.Method == http.MethodDelete {
-			ts.deleteTaskHandler(w, req, int(id))
-		} else if req.Method == http.MethodGet {
-			ts.getTaskHandler(w, req, int(id))
-		} else {
-			http.Error(w, fmt.Sprintf("expect method GET or DELETE at /task/<id>, got %v", req.Method), http.StatusMethodNotAllowed)
-			return
-		}
+		http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
 	}
 }
 
-func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+func (ts *taskServer) getReviewValue(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get 1xbetLink %s\n", req.URL.Path)
+
+	reviewValue := ts.store.GetReviewValue()
+	js, err := json.Marshal(reviewValue)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) get1winLink(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get 1win link %s\n", req.URL.Path)
+
+	winBetLink := ts.store.GetWinLink()
+	js, err := json.Marshal(winBetLink)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) getMelbetLink(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get Melbet link %s\n", req.URL.Path)
+
+	melBetLink := ts.store.GetMelbetLink()
+	js, err := json.Marshal(melBetLink)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) get1xbetLink(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get 1xbetLink %s\n", req.URL.Path)
+
+	betLink := ts.store.Get1XbetLink()
+	js, err := json.Marshal(betLink)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) setReviewValue(w http.ResponseWriter, req *http.Request) {
 	log.Printf("handling task create at %s\n", req.URL.Path)
 
-	// Types used internally in this handler to (de-)serialize the request and
-	// response from/to JSON.
-	type RequestTask struct {
-		Text string    `json:"text"`
-		Tags []string  `json:"tags"`
-		Due  time.Time `json:"due"`
-	}
-
-	type ResponseId struct {
-		Id int `json:"id"`
+	type RequestBody struct {
+		NewReviewValue bool `json:"newValue"`
 	}
 
 	// Enforce a JSON Content-Type.
@@ -90,130 +109,14 @@ func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request
 
 	dec := json.NewDecoder(req.Body)
 	dec.DisallowUnknownFields()
-	var rt RequestTask
+	var rt RequestBody
 	if err := dec.Decode(&rt); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
-	js, err := json.Marshal(ResponseId{Id: id})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling get all tasks at %s\n", req.URL.Path)
-
-	allTasks := ts.store.GetAllTasks()
-	js, err := json.Marshal(allTasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
-	log.Printf("handling get task at %s\n", req.URL.Path)
-
-	task, err := ts.store.GetTask(id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-
-	js, err := json.Marshal(task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
-	log.Printf("handling delete task at %s\n", req.URL.Path)
-
-	err := ts.store.DeleteTask(id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-	}
-}
-
-func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling delete all tasks at %s\n", req.URL.Path)
-	ts.store.DeleteAllTasks()
-}
-
-func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling tasks by tag at %s\n", req.URL.Path)
-
-	if req.Method != http.MethodGet {
-		http.Error(w, fmt.Sprintf("expect method GET /tag/<tag>, got %v", req.Method), http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := strings.Trim(req.URL.Path, "/")
-	pathParts := strings.Split(path, "/")
-	if len(pathParts) < 2 {
-		http.Error(w, "expect /tag/<tag> path", http.StatusBadRequest)
-		return
-	}
-	tag := pathParts[1]
-
-	tasks := ts.store.GetTasksByTag(tag)
-	js, err := json.Marshal(tasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling tasks by due at %s\n", req.URL.Path)
-
-	if req.Method != http.MethodGet {
-		http.Error(w, fmt.Sprintf("expect method GET /due/<date>, got %v", req.Method), http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := strings.Trim(req.URL.Path, "/")
-	pathParts := strings.Split(path, "/")
-
-	badRequestError := func() {
-		http.Error(w, fmt.Sprintf("expect /due/<year>/<month>/<day>, got %v", req.URL.Path), http.StatusBadRequest)
-	}
-	if len(pathParts) != 4 {
-		badRequestError()
-		return
-	}
-
-	year, err := strconv.Atoi(pathParts[1])
-	if err != nil {
-		badRequestError()
-		return
-	}
-	month, err := strconv.Atoi(pathParts[2])
-	if err != nil || month < int(time.January) || month > int(time.December) {
-		badRequestError()
-		return
-	}
-	day, err := strconv.Atoi(pathParts[3])
-	if err != nil {
-		badRequestError()
-		return
-	}
-
-	tasks := ts.store.GetTasksByDueDate(year, time.Month(month), day)
-	js, err := json.Marshal(tasks)
+	ts.store.ChangeReviewValue(rt.NewReviewValue)
+	js, err := json.Marshal("Success")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -225,9 +128,7 @@ func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	server := NewTaskServer()
-	mux.HandleFunc("/task/", server.taskHandler)
-	mux.HandleFunc("/tag/", server.tagHandler)
-	mux.HandleFunc("/due/", server.dueHandler)
+	mux.HandleFunc("/", server.dataHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
 }
