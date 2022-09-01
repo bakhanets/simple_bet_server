0eb8993dc45a174d054d080e6eb9b0dbecc40c68
Rozhkov_Bogdan <lomus.br@gmial.com>
2022-09-01 22:52:15 +0300

    minor changes

--- betting_store/betting_store.go
+++ betting_store/betting_store.go
@@ -4,49 +4,58 @@ import (
 	"sync"
 )
 
-type BettingData struct {
-	XBetLink   string `json:"1xBetLink"`
-	MelbetLink string `json:"MelbetLink"`
-	WinLink    string `json:"WinLink"`
-
-	AppIsOnReview bool `json:"onReview"`
-}
-
 type BettingStore struct {
 	sync.Mutex
-
-	bettingData BettingData
+	XBetLink         string
+	MelBetLink       string
+	WinLink          string
+	XBetReviewLink   string
+	MelBetReviewLink string
+	WinReviewLink    string
+	AppIsOnReview    bool
 }
 
-func New() *BettingStore {
-	ts := &BettingStore{}
-	ts.bettingData.XBetLink = "https://refpa6627021.top/L?tag=d_871311m_1599c_&site=871311&ad=1599"
-	ts.bettingData.MelbetLink = "https://refpa31055.top/L?tag=s_875293m_18637c_&site=875293&ad=18637"
-	ts.bettingData.WinLink = "https://1wlint.top/?open=register#x6ev"
-	ts.bettingData.AppIsOnReview = false
-	return ts
+func NewBettingStore() *BettingStore {
+	return &BettingStore{
+		XBetLink:         "https://refpa6627021.top/L?tag=d_871311m_1599c_&site=871311&ad=1599",
+		MelBetLink:       "https://refpa31055.top/L?tag=s_875293m_18637c_&site=875293&ad=18637",
+		WinLink:          "https://1wlint.top/?open=register#x6ev",
+		XBetReviewLink:   "https://google.com",
+		MelBetReviewLink: "https://google.com",
+		WinReviewLink:    "https://google.com",
+		AppIsOnReview:    true,
+	}
 }
 
-func (ts *BettingStore) ChangeReviewValue(newOnReviewValue bool) bool {
-	ts.Lock()
-	defer ts.Unlock()
-
-	ts.bettingData.AppIsOnReview = newOnReviewValue
-	return ts.bettingData.AppIsOnReview
+func (bs *BettingStore) ChangeReviewValue(newOnReviewValue bool) {
+	bs.Lock()
+	defer bs.Unlock()
+	bs.AppIsOnReview = newOnReviewValue
 }
 
-func (ts *BettingStore) GetReviewValue() bool {
-	return ts.bettingData.AppIsOnReview
+func (bs *BettingStore) getReviewValue() bool {
+	bs.Lock()
+	defer bs.Unlock()
+	return bs.AppIsOnReview
 }
 
-func (ts *BettingStore) GetMelbetLink() string {
-	return ts.bettingData.MelbetLink
+func (bs *BettingStore) GetMelBetLink() string {
+	if bs.getReviewValue() {
+		return bs.MelBetReviewLink
+	}
+	return bs.MelBetLink
 }
 
-func (ts *BettingStore) Get1XbetLink() string {
-	return ts.bettingData.XBetLink
+func (bs *BettingStore) Get1XBetLink() string {
+	if bs.getReviewValue() {
+		return bs.XBetReviewLink
+	}
+	return bs.XBetLink
 }
 
-func (ts *BettingStore) GetWinLink() string {
-	return ts.bettingData.WinLink
+func (bs *BettingStore) GetWinLink() string {
+	if bs.getReviewValue() {
+		return bs.WinReviewLink
+	}
+	return bs.WinLink
 }
--- main.go
+++ main.go
@@ -10,91 +10,64 @@ import (
 	"os"
 )
 
-type taskServer struct {
-	store *betting_store.BettingStore
-}
+var store = betting_store.NewBettingStore()
 
-func NewTaskServer() *taskServer {
-	store := betting_store.New()
-	return &taskServer{store: store}
-}
-
-func (ts *taskServer) dataHandler(w http.ResponseWriter, req *http.Request) {
+func dataHandler(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/getMelbetLink" {
-		ts.getMelbetLink(w, req)
+		getMelbetLink(w, req)
 	} else if req.URL.Path == "/get1XbetLink" {
-		ts.get1xbetLink(w, req)
+		get1xbetLink(w, req)
 	} else if req.URL.Path == "/get1winLink" {
-		ts.get1winLink(w, req)
-	} else if req.URL.Path == "/getReviewValue" {
-		ts.getReviewValue(w, req)
+		get1winLink(w, req)
 	} else if req.URL.Path == "/setReviewValue" {
-		ts.setReviewValue(w, req)
+		setReviewValue(w, req)
 	} else {
 		http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
 		return
 	}
 }
 
-func (ts *taskServer) getReviewValue(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling get 1xbetLink %s\n", req.URL.Path)
-
-	reviewValue := ts.store.GetReviewValue()
-	js, err := json.Marshal(reviewValue)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func (ts *taskServer) get1winLink(w http.ResponseWriter, req *http.Request) {
+func get1winLink(w http.ResponseWriter, req *http.Request) {
 	log.Printf("handling get 1win link %s\n", req.URL.Path)
 
-	winBetLink := ts.store.GetWinLink()
+	winBetLink := store.GetWinLink()
 	js, err := json.Marshal(winBetLink)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	_, _ = w.Write(js)
 }
 
-func (ts *taskServer) getMelbetLink(w http.ResponseWriter, req *http.Request) {
+func getMelbetLink(w http.ResponseWriter, req *http.Request) {
 	log.Printf("handling get Melbet link %s\n", req.URL.Path)
 
-	melBetLink := ts.store.GetMelbetLink()
+	melBetLink := store.GetMelBetLink()
 	js, err := json.Marshal(melBetLink)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	_, _ = w.Write(js)
 }
 
-func (ts *taskServer) get1xbetLink(w http.ResponseWriter, req *http.Request) {
+func get1xbetLink(w http.ResponseWriter, req *http.Request) {
 	log.Printf("handling get 1xbetLink %s\n", req.URL.Path)
 
-	betLink := ts.store.Get1XbetLink()
+	betLink := store.Get1XBetLink()
 	js, err := json.Marshal(betLink)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	_, _ = w.Write(js)
 }
 
-func (ts *taskServer) setReviewValue(w http.ResponseWriter, req *http.Request) {
+func setReviewValue(w http.ResponseWriter, req *http.Request) {
 	log.Printf("handling task create at %s\n", req.URL.Path)
-
-	type RequestBody struct {
-		NewReviewValue bool `json:"newValue"`
-	}
-
 	// Enforce a JSON Content-Type.
 	contentType := req.Header.Get("Content-Type")
 	mediatype, _, err := mime.ParseMediaType(contentType)
@@ -109,26 +82,30 @@ func (ts *taskServer) setReviewValue(w http.ResponseWriter, req *http.Request) {
 
 	dec := json.NewDecoder(req.Body)
 	dec.DisallowUnknownFields()
-	var rt RequestBody
+	var rt struct {
+		NewReviewValue bool `json:"newValue"`
+	}
 	if err := dec.Decode(&rt); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	ts.store.ChangeReviewValue(rt.NewReviewValue)
+	store.ChangeReviewValue(rt.NewReviewValue)
 	js, err := json.Marshal("Success")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	_, _ = w.Write(js)
 }
 
 func main() {
-	mux := http.NewServeMux()
-	server := NewTaskServer()
-	mux.HandleFunc("/", server.dataHandler) //http://127.0.0.1:8000/get1winLink to get data
-
-	log.Fatal(http.ListenAndServe(":8000"+os.Getenv("SERVERPORT"), mux))
+	port := ":8000"
+	osPort, ok := os.LookupEnv("ServerPort")
+	if ok {
+		port = ":" + osPort
+	}
+	http.HandleFunc("/", dataHandler) //http://127.0.0.1:8000/get1winLink to get data
+	log.Fatal(http.ListenAndServe(port, nil))
 }
